c65fb1f016cfb336c840de2237f1db736416fa5a
carreb <caleb@domyjobfor.me>
2022-11-10 22:17:47 -0500

    Functional booking section

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,400 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="body" :class="{ noscroll: bookingModalOpen }" id="About" ref="about">
+    <BookForm @closebookingmodal="closeBookingModal" :bookingModalOpenProp="bookingModalOpen"></BookForm>
+    <header class="header">
+      <ul>
+        <li>
+          <p><a href="/#About">About Us</a></p>
+        </li>
+        <li>
+          <p><a href="/#Talent">Talent Show</a></p>
+        </li>
+      </ul>
+    </header>
+    <div class="initialScreen">
+      <div class="typographybg">
+        <h1 class="bigtypography">WHAT IS THE AFC?</h1>
+      </div>
+      <div class="aboutbox">
+        <h1 class="clubname">AUDITORIUM FACILITIES CREW</h1>
+        <br>
+        <h2 class="gradient-text subheadingReposition">ABOUT US</h2>
+        <p class="clubinfo">The AFC is responsible for working the more technical parts of the auditorium. We help out with assemblies, extracurricular activities, and more by setting up the stage lights, speakers and other equipment. We have about 20 crew members, if you need us for your show, click the button below to book!</p>
+        <br>
+        <br>
+        <br>
+        <button class="applybtn" @click="openBookingModal">BOOK US</button>
+      </div>
+      <div class="backgroundimg">
+        <img src="https://picsum.photos/1280/720?random=1" class="backgroundimg">
+      </div>
+      <div class="scrollReminder">
+        <p>Scroll to see more</p>
+      </div>
+    </div>
+    <div class="secondScreen">
+      <div class="typographybg2">
+        <h1 class="bigtypography2">TALENT SHOW SIGNUPS</h1>
+      </div>
+      <div class="aboutbox2" id="Talent">
+        <h1 class="clubname">TALENT SHOW REGISTRATION</h1>
+        <br>
+        <h2 class="gradient-text">DECEMBER 23</h2>
+        <p class="clubinfo">Our normal Winter Assembly has been replaced with a Talent Show! Click the button below to
+          audition for an act!</p>
+        <button class="applybtn2">GOT TALENT?</button>
+      </div>
+      <div class="backgroundimg2">
+        <img src="https://picsum.photos/1280/720?random=2" class="backgroundimg">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BookForm from "./components/BookForm.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BookForm
+  },
+  data() {
+    return {
+      bookingModalOpen: false
+    }
+  },
+  methods: {
+    openBookingModal() {
+      window.location.href = "#About"
+      this.bookingModalOpen = true
+    },
+    closeBookingModal() {
+      window.location.href = "#"
+      this.bookingModalOpen = false; 
+    }
+  }, 
+  watch: {
+    bookingModalOpen: function() {
+      if (this.bookingModalOpen) {
+        return document.documentElement.style.overflow = 'hidden'
+      } else {
+        return document.documentElement.style.overflow = 'auto'
+      }
+    }   
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url('https://fonts.googleapis.com/css2?family=Questrial&display=swap');
+
+/* buttons.css */
+button {
+  padding: 10px 20px;
+  background: linear-gradient(-45deg, rgb(229, 157, 22), #57acdc);
+  background-size: 400% 400%;
+  border: none;
+  border-radius: 0.1rem;
+  font-family: 'Questrial', sans-serif;
+  font-size: 1.2rem;
+  font-weight: 400;
+  color: white;
+  z-index: 100;
+  box-shadow: 6px 6px 5px rgba(0, 0, 0, 0.596);
+  animation: reverse-gradient 1s forwards cubic-bezier(0.07, 0.5, 0.2, 1);
+}
+
+button:hover {
+  animation: gradient 1s forwards cubic-bezier(0.07, 0.5, 0.2, 1);
+  cursor: pointer;
+  z-index: 100;
+}
+
+#email {
+  display: none;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+
+  100% {
+    background-position: 100% 50%;
+  }
+}
+
+@keyframes reverse-gradient {
+  0% {
+    background-position: 100% 50%;
+  }
+
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
+button:disabled {
+  cursor: not-allowed;
+}
+
+
+/* contactpagestyles.css */
+.contact {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  align-self: center;
+}
+
+.wrappercontactpg {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+/* global.css */
+:root {
+  /* general random colors */
+  --mciafcmagenta: #e91e63;
+  --mciafcdarkmagenta: #c21858;
+  --mciafcpurple: #9c2780;
+  --mciafcdeeppurple: #572780;
+  --mciafcblue: #272ab0;
+  --mciafcnavy: #276880;
+  --mciafcmint: #57dcbe;
+  --mciafcgreen: #60c689;
+  /* main palette */
+  --mciafcorange: rgb(229, 157, 22);
+  --mciafcsky: #57acdc;
+  --mciafcgray: #313030;
+  --mciafcblack: #1a1a1a;
+}
+
+a {
+  font-family: 'Poppins', sans-serif;
+  color: lightblue;
+  text-decoration: none;
+}
+
+a:visited {
+  color: lightblue;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
+
+/* header.css */
+li {
+  float: left;
+}
+
+li p {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 16px;
+  padding-top: 0;
+  text-decoration: none;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
+.header {
+  position: fixed;
+  background-color: var(--mciafcblack);
+  width: 100%;
+  padding-top: 10px;
+  padding-bottom: -30px;
+  z-index: 100;
+}
+
+/* initialscreen.css */
+
+body {
+  background-color: var(--mciafcblack);
+  color: #FFF;
+  font-family: 'Questrial', sans-serif;
+  z-index: 2;
+  overflow-x: hidden;
+  margin-left: 0;
+  margin-top: 0;
+}
+
+.clubname {
+  color: var(--mciafcorange);
+}
+
+.backgroundimg {
+  position: absolute;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+
+.applybtn {
+  position: absolute;
+  margin: auto;
+  left: 0;
+  right: 0;
+  bottom: 50px;
+}
+
+.aboutbox {
+  position: absolute;
+  margin: 0;
+  width: 400px;
+  height: 600px;
+  top: 15%;
+  line-height: 1.2;
+  left: 5%;
+  background-color: var(--mciafcblack);
+  z-index: 10;
+  text-align: center;
+  padding: 15px;
+  box-shadow: 2px 2px rgba(0, 0, 0, 0.345);
+}
+
+.clubinfo {
+  font-size: 25px;
+}
+
+.gradient-text {
+  background-image: linear-gradient(-45deg, rgb(229, 157, 22), #57acdc);
+  background-size: 200% 200%;
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+}
+
+.subheadingReposition {
+  position: absolute;
+  margin: auto;
+  left: 0;
+  right: 0;
+  top: 150px;
+}
+
+
+
+/* Scroll Reminder */
+
+.scrollReminder {
+  overflow: hidden;
+  position: absolute;
+  bottom: 20px;
+  background: linear-gradient(-45deg, rgb(229, 157, 22), #57acdc);
+  background-position: 100% 50%;
+  background-size: 200% 200%;
+  z-index: 1;
+  right: 30px;
+  padding-left: 20px;
+  padding-right: 20px;
+  /* animation: scrollReminderPopIn 600ms forwards 5000ms;
+  transform: translateX(120%); */
+}
+
+
+.bigtypography {
+  /* position: absolute;
+    top: 40%;
+    right: 2%; */
+  font-size: 100px;
+  background-position: 50% 200%;
+  background-image: linear-gradient(-45deg, rgb(229, 157, 22), #57acdc);
+  background-size: 200% 200%;
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  text-shadow: 10px 5px 1px rgba(255, 255, 255);
+}
+
+.typographybg {
+  position: absolute;
+  top: 39%;
+  right: 2%;
+  padding-left: 20px;
+  padding-right: 20px;
+  width: fit-content;
+  text-align: center;
+  margin: 0;
+  height: fit-content;
+}
+
+
+
+@keyframes scrollReminderPopIn {
+  0% {
+    transform: translateX(120%);
+  }
+
+  100% {
+    transform: translateX(0%);
+  }
+}
+
+/* secondscreen.css */
+.aboutbox2 {
+  position: absolute;
+  margin: 0;
+  width: 400px;
+  height: 600px;
+  top: 130%;
+  right: 5%;
+  background-color: var(--mciafcblack);
+  z-index: 10;
+  text-align: center;
+  padding: 15px;
+  line-height: 1.2;
+  box-shadow: 2px 2px rgba(0, 0, 0, 0.345);
+}
+
+.backgroundimg2 {
+  position: absolute;
+  z-index: -1;
+  width: 100%;
+  top: 115%;
+  height: 100%;
+  margin: 0;
+}
+
+.applybtn2 {
+  position: absolute;
+  bottom: 50px;
+  left: 33%;
+}
+
+.bigtypography2 {
+  /* position: absolute;
+    top: 40%;
+    right: 2%; */
+  font-size: 100px;
+  background-position: 50% 200%;
+  background-image: linear-gradient(-45deg, rgb(229, 157, 22), #57acdc);
+  background-size: 200% 200%;
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  text-shadow: 10px 5px 1px rgba(255, 255, 255);
+}
+
+.typographybg2 {
+  position: absolute;
+  top: 147%;
+  left: 2%;
+  padding-left: 20px;
+  padding-right: 20px;
+  width: fit-content;
+  max-width: 60%;
+  text-align: center;
+  margin: 0;
+  height: fit-content;
 }
 </style>
---
+++ src/components/BookForm.vue
@@ -0,0 +1,401 @@
+<template>
+    <div class="bookModal" :class="{ scrollable: modalScrollEnabled }" ref="bookModal" v-if="bookingModalOpenProp">
+        <h1 v-if="bookingModalBookStage != 5">Book the AFC</h1>
+        <p v-if="![2, 5].includes(bookingModalBookStage)">Fields marked with * are required. You'll be able to review your information before you submit.</p>
+        <!-- Organizer Info -->
+        <div class="OrganizerInfo" v-if="bookingModalBookStage == 0">
+            <h2>Organizer Information</h2>
+            <p>Some information about you, so we can get in contact in case we need any extra details or you need to let us know
+                about changes.</p>
+            <h3>Name*</h3>
+            <input type="text" name="Organizer Name" ref="organizerName" placeholder="Organizer/Your Name">
+            <h3>Email*</h3>
+            <input type="text" name="Organizer Email" ref="organizerEmail" placeholder="Organizer/Your Email">
+            <h3>Phone Number</h3>
+            <p>We only call or text if it's really needed.</p>
+            <input type="text" name="Organizer Phone Number" ref="organizerPhoneNumber" placeholder="Organizer/Your Phone Number">
+        </div>
+        <!-- Organization Info -->
+        <div class="OrganizationInfo" v-if="bookingModalBookStage == 1">
+            <h2>Organization Information</h2>
+                <h3>Organization Name*</h3>
+                <input type="text" name="Organization Name" ref="organizationName" placeholder="(e.g AFC, SAC, etc.)">
+        </div>
+        <!-- Event info -->
+        <div class="EventInfo" v-if="bookingModalBookStage == 2">
+            <h2>Event Information</h2>
+            <h3>Event Name*</h3>
+            <input type="text" name="Event Name" ref="eventNameRef" placeholder="(e.g Terry Fox Assembly)">
+            <h3>Event Start*</h3>
+            <p>This should not include the time we will use to setup.</p>
+            <input type="datetime-local" name="Event Start Date" ref="eventStartDate" class="dateselect">
+            <h3>Event End*</h3>
+            <p>This should not include the time we will use for teardown.</p>
+            <input type="datetime-local" name="Event End Date" ref="eventEndDate" class="dateselect">
+            <h3>Event Location</h3>
+            <p>Where your event is being held at.</p>
+            <input type="text" name="Event Location" ref="eventLocation" placeholder="(e.g Cafeteria, Library, Gym)">
+        </div>
+        <!-- Crew Info -->
+        <div class="CrewInfo" v-if="bookingModalBookStage == 3">
+            <h2>Crew Information</h2>
+            <h3>Crew Members Needed</h3>
+            <p>The amount of crew members needed, we have around 20 including 3 execs. Set to 0 for as many as possible.</p>
+            <input type="number" name="CrewMembersNeeded" max="21" min="0" value="0" ref="crewMembersNeeded">
+            <h3>Paid Job</h3>
+            <p>Tick the box if the crew members that will be present will be getting paid for their work. If you're an in-school
+                organization, you should probably leave the box unticked.</p>
+            <input type="checkbox" name="Paid Job" ref="paidJob">
+        </div>
+        <!-- Additional Info -->
+        <div class="AdditionalInfo" v-if="bookingModalBookStage == 4">
+            <h2>Additional Information</h2>
+            <p>Any additional information that you feel needs to be specified can go below.</p>
+            <textarea name="Additional Info" ref="additionalInfo"
+                placeholder="(e.g Sound equipment should be set up at the back of the auditorium), other useful things to put here are the types of equipment we will need, we've got plenty so just let us know and we'll get back and let you know if we have it."></textarea>
+        </div>
+        <!-- Verify -->
+        <div class="verifyInfo" v-if="bookingModalBookStage == 5">
+            <h1>Verify your Info:</h1>
+            <p>Click any of the headings to go back and edit</p>
+            <h3 style="text-decoration: underline; cursor: pointer;" @click="startReview(0)">Organizer Info</h3>
+            <p>Organizer Name: {{ organizerName }}</p>
+            <p>Organizer Email: {{ organizerEmail }}</p>
+            <p v-if="organizerPhoneNumber != 'Not provided'">Organizer Phone Number: {{ organizerPhoneNumber }}</p>
+            <p>Organization Name: {{ organizationName }}</p>
+            <h3 style="text-decoration: underline; cursor: pointer;" @click="startReview(2)">Event Info</h3>
+            <p>Event Name: {{ eventName }}</p>
+            <p>Takes Place: {{ dateRange(eventStartDate, eventEndDate) }}</p>
+            <p>Event Location: {{ eventLocation }}</p>
+            <h3 style="text-decoration: underline; cursor: pointer;" @click="startReview(3)">Crew Info</h3>
+            <p>Crew Members Needed: {{ employeesNeeded(crewMembersNeeded) }}</p>
+            <p>Paid Job: {{ paidJob }}</p>
+            <h3 style="text-decoration: underline; cursor: pointer;" @click="startReview(4)" v-if="additionalInfo != 'No additional details specified.'">Additional Info</h3>
+            <p v-if="additionalInfo != 'No additional details specified.'">{{ additionalInfo }}</p>
+        </div>
+        <br>
+        <button class="continuebutton" @click="nextModalPage" v-if="!reviewMode" ref="continuebutton">Continue</button>
+        <button class="continuebutton" @click="finishReviewing" v-else>Finish Reviewing This Section</button>
+        <p style="color: red;" v-if="requiredFieldCheckFailed" class="fieldCheckText" ref="fieldCheckText">Please fill out all required fields</p>
+        <p class="pagenumber">Page {{ bookingModalBookStage + 1 }} / 6</p>
+    </div>
+    <div class="darkenbackground" v-if="bookingModalOpenProp" @click="this.$emit('closebookingmodal')" :class="{ noscroll: bookingModalOpenProp }"></div>
+</template>
+
+<script>
+export default {
+    name: 'BookForm',
+    data() {
+        return {
+            // State managers
+            bookingModalOpen: false,
+            bookingModalBookStage: 0, // 0 = organizer info, 1 = organization info, 2 = event info, 3 = crew info, 4 = additional info, 5 = confirmation window
+            requiredFieldCheckFailed: false,
+            reviewMode: false,
+            modalScrollEnabled: false,
+
+            // Booking Info
+            // Organizer Info (Contact Info Stuff)
+            organizerName: "", // req
+            organizerEmail: "", // req
+            organizerPhoneNumber: "", // not required
+
+            // Organization Info
+            organizationName: "", // req
+
+            // Event Info
+            eventName: "", // req
+            eventStartDate: new Date(), // req
+            eventEndDate: new Date(), // req
+            eventLocation: "", // partially required, defaults to MCI Auditorium if left blank 
+
+            // Crew information (Just extra info for us)
+            membersNeeded: 0, // partially required, if left at 0 will show in the dashboard as "As many as possible"
+            paidJob: false, // only usually paid by outside of school organizations like epo
+
+            // Additional Info (big body of text)
+            additionalInfo: "" // Will probably be edited by an exec
+        }
+    },
+    props: {
+        bookingModalOpenProp: Boolean
+    },
+    emits: ['closebookingmodal'],
+    methods: {
+        nextModalPage() {
+            if (this.bookingModalBookStage === 0) {
+                this.organizerName = this.$refs.organizerName.value
+                this.organizerEmail = this.$refs.organizerEmail.value
+                this.organizerPhoneNumber = this.$refs.organizerPhoneNumber.value
+                if (!this.$refs.organizerPhoneNumber.value) {
+                    this.organizerPhoneNumber = "Not provided"
+                }
+                if (!this.$refs.organizerName.value || !this.$refs.organizerEmail.value) {
+                    this.requiredFieldCheckFailed = true
+                } else {
+                    this.bookingModalBookStage++
+                    this.requiredFieldCheckFailed = false
+                    return
+                }
+            } else
+            if (this.bookingModalBookStage === 1) {
+                this.organizationName = this.$refs.organizationName.value
+                if (!this.$refs.organizationName.value) {
+                    this.requiredFieldCheckFailed = true
+                } else {
+                    this.bookingModalBookStage++
+                    this.requiredFieldCheckFailed = false
+                    return
+                }
+            } else
+            if (this.bookingModalBookStage === 2) {
+                this.eventName = this.$refs.eventNameRef.value
+                this.eventStartDate = this.$refs.eventStartDate.value
+                this.eventEndDate = this.$refs.eventEndDate.value
+                this.eventLocation = this.$refs.eventLocation.value
+                console.log(this.$refs.eventNameRef.value)
+                console.table({startDate: this.eventStartDate, endDate: this.eventEndDate})
+                if (!this.$refs.eventLocation.value) {
+                    this.eventLocation = "MCI Auditorium"
+                }
+                if (!this.$refs.eventNameRef.value || !this.$refs.eventStartDate.value.toString() || !this.$refs.eventEndDate.value.toString()) {
+                    this.requiredFieldCheckFailed = true
+                } else {
+                    this.bookingModalBookStage++
+                    this.requiredFieldCheckFailed = false
+                    return
+                }
+            } else
+            if (this.bookingModalBookStage === 3) {
+                this.crewMembersNeeded = this.$refs.crewMembersNeeded.value
+                this.paidJob = this.$refs.paidJob.checked
+                console.log(this.$refs.paidJob.checked)
+                if (!this.$refs.crewMembersNeeded) {
+                    this.requiredFieldCheckFailed = true
+                } else {
+                    this.bookingModalBookStage++
+                    this.requiredFieldCheckFailed = false
+                    return
+                }
+            } else 
+            if (this.bookingModalBookStage === 4) {
+                this.additionalInfo = this.$refs.additionalInfo.value
+                if (!this.$refs.additionalInfo.value) {
+                    this.additionalInfo = "No additional details specified."
+                }
+                this.bookingModalBookStage++
+                this.requiredFieldCheckFailed = false
+                this.modalScrollEnabled = true
+                return
+            } else
+            if (this.bookingModalBookStage === 5) {
+                this.$refs.continuebutton.disabled = true;
+                let body = {
+                    "organizerName": this.organizerName,
+                    "organizerContactEmail": this.organizerEmail,
+                    "organizerContactNumber": this.organizerPhoneNumber,
+                    "organizationName": this.organizationName,
+                    "gigName": this.eventName,
+                    "gigStartDate": this.eventStartDate,
+                    "gigEndDate": this.eventEndDate,
+                    "registeredByOrganizer": true,
+                    "employeesNeeded": this.crewMembersNeeded,
+                    "paidJob": this.paidJob,
+                    "additionalInformation": this.additionalInfo
+                }
+                fetch("https://api.mciafc.com/gigs/schedule", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(body)
+                }).then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.success == true) {
+                        this.$emit("closebookingmodal")
+                    }
+                })
+            }
+        },
+        startReview(page) {
+            this.bookingModalBookStage = page;
+            this.modalScrollEnabled = false
+            this.reviewMode = true
+        },
+        finishReviewing() {
+            if (this.bookingModalBookStage === 0) {
+                this.organizerName = this.$refs.organizerName.value
+                this.organizerEmail = this.$refs.organizerEmail.value
+                this.organizerPhoneNumber = this.$refs.organizerPhoneNumber.value
+                if (!this.$refs.organizerPhoneNumber.value) {
+                    this.organizerPhoneNumber = "Not provided"
+                }
+                if (!this.$refs.organizerName.value || !this.$refs.organizerEmail.value) {
+                    this.requiredFieldCheckFailed = true
+                } else {
+                    this.bookingModalBookStage = 1
+                    this.requiredFieldCheckFailed = false
+                    return
+                }
+            } else
+            if (this.bookingModalBookStage === 1) {
+                this.organizationName = this.$refs.organizationName.value
+                if (!this.$refs.organizationName.value) {
+                    this.requiredFieldCheckFailed = true
+                } else {
+                    this.bookingModalBookStage = 5
+                    this.modalScrollEnabled = true
+                    this.reviewMode = false
+                    this.requiredFieldCheckFailed = false
+                    return
+                }
+            } else
+            if (this.bookingModalBookStage === 2) {
+                this.eventName = this.$refs.eventNameRef.value
+                this.eventStartDate = this.$refs.eventStartDate.value
+                this.eventEndDate = this.$refs.eventEndDate.value
+                this.eventLocation = this.$refs.eventLocation.value
+                console.log(this.$refs.eventNameRef.value)
+                console.table({startDate: this.eventStartDate, endDate: this.eventEndDate})
+                if (!this.$refs.eventLocation.value) {
+                    this.eventLocation = "MCI Auditorium"
+                }
+                if (!this.$refs.eventNameRef.value || !this.$refs.eventStartDate.value.toString() || !this.$refs.eventEndDate.value.toString()) {
+                    this.requiredFieldCheckFailed = true
+                } else {
+                    this.bookingModalBookStage = 5
+                    this.modalScrollEnabled = true
+                    this.reviewMode = false
+                    this.requiredFieldCheckFailed = false
+                    return
+                }
+            } else
+            if (this.bookingModalBookStage === 3) {
+                this.crewMembersNeeded = this.$refs.crewMembersNeeded.value
+                this.paidJob = this.$refs.paidJob.checked
+                console.log(this.$refs.paidJob.checked)
+                if (!this.$refs.crewMembersNeeded) {
+                    this.requiredFieldCheckFailed = true
+                } else {
+                    this.bookingModalBookStage = 5
+                    this.modalScrollEnabled = true
+                    this.reviewMode = false
+                    this.requiredFieldCheckFailed = false
+                    return
+                }
+            } else 
+            if (this.bookingModalBookStage === 4) {
+                this.additionalInfo = this.$refs.additionalInfo.value
+                if (!this.$refs.additionalInfo.value) {
+                    this.additionalInfo = "No additional details specified."
+                }
+                this.bookingModalBookStage = 5
+                this.modalScrollEnabled = true
+                this.reviewMode = false
+                this.requiredFieldCheckFailed = false
+                this.modalScrollEnabled = true
+                return
+            }
+        }
+    },
+    computed: {
+        dateRange() {
+            return function (start, end) {
+                let startString = new Date(start).toDateString()
+                // let endString = new Date(end).toDateString()
+                let startMinutes = new Date(start).getMinutes()
+                let startHours = new Date(start).getHours()
+                let endMinutes = new Date(end).getMinutes()
+                let endHours = new Date(end).getHours()
+                if (startMinutes < 10) {
+                    startMinutes = `0${startMinutes}`
+                }
+                if (endMinutes < 10) {
+                    endMinutes = `0${endMinutes}`
+                }
+                return `${startString} @ ${startHours}:${startMinutes} - ${endHours}:${endMinutes}`
+            }
+        },
+        employeesNeeded() {
+            return function (amountSpecified) {
+                if (amountSpecified > 0) {
+                    return `${amountSpecified}`
+                }
+                return "As many as possible."
+            }
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+    input {
+        padding: 10px;
+        font-size: 15px;
+    }
+    textarea {
+        padding: 10px;
+        font-size: 15px;
+        resize: none;
+        height: 200px;
+        width: 80%
+    }
+
+    .bookModal {
+        position: absolute;
+        margin: auto;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        text-align: center;
+        height: 800px;
+        width: 600px;
+        background-color: #191919;
+        font-size: 19px;
+        z-index: 20000000;
+        box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.6);
+        padding: 15px;
+        border-radius: 1rem;
+        overflow: hidden;
+    }
+
+
+    .darkenbackground {
+        z-index: 10000;
+        width: 100%;
+        height: 100%;
+        background-color: #000000;
+        opacity: 0.6;
+        position: absolute;
+        margin: 0;
+        top: 0;
+        left: 0;
+    }
+
+    .scrollable {
+        overflow-y: scroll !important;
+    }
+
+    .continuebutton {
+        position: absolute;
+        margin: auto;
+        left: 0;
+        right: 0;
+        bottom: 80px;
+    }
+    .pagenumber {
+        position: absolute;
+        margin: auto;
+        left: 0;
+        right: 0;
+        bottom: 40px;
+    }
+    .fieldCheckText {
+        position: absolute;
+        margin: auto;
+        left: 0;
+        right: 0;
+        bottom: 20px;
+    }
+</style>
